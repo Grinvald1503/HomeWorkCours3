baaeb09ddbbd13f24f1259a62773ebad460b909a
Aleksandr <aleksanches123@mail.ru>
2023-02-05 18:16:53 +0300

    начало

---
+++ src/main/java/com/example/homeworkcourse3/HomeWorkCourse3Application.java
@@ -0,0 +1,13 @@
+package com.example.homeworkcourse3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkCourse3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkCourse3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworkcourse3/HomeWorkCourse3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkcourse3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkCourse3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
